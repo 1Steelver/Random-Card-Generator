565ca72ad926d4f02ab9443d50bbcea84285d170
Steelver <1steelver@gmail.com>
2022-05-05 12:00:53 +0000

    la carta

--- src/app.js
+++ src/app.js
@@ -5,6 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+//window.onload = function() {
+//console.log("Hello Rigo from the console!");
+//};
+
+//let random = () => {
+//let fig = ["♦", "♥", "♠", "♣"];
+//let num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+//let generaNum = Math.floor(Math.random() * num.length);
+//let generaFig = Math.floor(Math.random() * fig.length);
+//window.onload = function() {
+//document.getElementById("up").innerHTML = generaFig;
+//document.getElementById("mid").innerHTML = generaNum;
+//  document.getElementById("down").innerHTML = generaFig;
+//};
+//};
